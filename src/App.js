1d157895bc1b33616bea8ddb6b20e9dc82e4a5b5
Anna Oginskaya <a.oginskaya@sana-commerce.com>
2020-07-10 18:34:12 +0300

    CountryPicker implemented

--- src/App.js
+++ src/App.js
@@ -7,21 +7,30 @@ import { fetchData } from './api';
 class App extends React.Component {
     state = {
         data: {},
+        country: '',
     }
 
-    async componentDidMount(){
+    async componentDidMount() {
         const fetchedData = await fetchData();
         this.setState({ data: fetchedData });
     }
 
-    render(){
-        const { data } = this.state;
+    handleCountryChange = async (country) => {
+        const fetchedData = await fetchData(country);
+        this.setState({ 
+            data: fetchedData,
+            country: country 
+        });
+    }
 
-        return(
+    render() {
+        const { data, country } = this.state;
+
+        return (
             <div className={styles.container}>
-                <Cards data={data}/>
-                <CountryPicker />
-                <Chart />
+                <Cards data={data} />
+                <CountryPicker handleCountryChange={this.handleCountryChange} />
+                <Chart data={data} country={country} />
             </div>
         )
     }
